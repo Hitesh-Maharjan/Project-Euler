cb788b1525edae55fd20f211a25ed30ff43f96de
Hitesh-Maharjan <hiteshmaharjan9@gmail.com>
2020-05-29 17:15:20 +0545

    2.0

---
+++ 2/2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 0, b = 1, c = 0, i, sum = 0;
+    for (i = 0; c < 4000000 ; i++)
+    {
+        c = a + b;
+        if ((c % 2)== 0 && (c < 4000000) )
+        {
+            sum += c;
+        }
+        a = b;
+        b = c;
+    }
+    printf("%i", sum);
+    return 0;
+}
